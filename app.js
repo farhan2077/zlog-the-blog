c8acf6e6ce5f9b787bf8446941ef26a52d69c52f
Farhan Bin Amin <farhanbinamin@gmail.com>
2020-11-14 00:23:52 +0600

    complete UI overhaul + added login and signup page

--- app.js
+++ app.js
@@ -1,39 +1,49 @@
-const express = require('express');
-const morgan = require('morgan');
-const mongoose = require('mongoose');
+const express = require("express");
+const morgan = require("morgan");
+const mongoose = require("mongoose");
 
-const { findByIdAndDelete } = require('./models/blog');
+const { findByIdAndDelete } = require("./models/blog");
 
-const blogRoutes = require('./routes/blogRoutes')
+const blogRoutes = require("./routes/blogRoutes");
 
 // express app
 const app = express();
-app.use(express.static(__dirname + '/public'));
-app.use(express.urlencoded({ extended: true }));    // accept form data
-app.use(morgan('dev'));
+app.use(express.static(__dirname + "/public"));
+app.use(express.urlencoded({ extended: true })); // accept form data
+app.use(morgan("dev"));
 
 // mongodb
-const dbURI = 'mongodb+srv://testUser:testUser12345678@nodetutblog.kxfwi.mongodb.net/node-blog-test?retryWrites=true&w=majority'
-mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(result => app.listen(3000))
-  .catch(err => console.log(err));
+const dbURI =
+    "mongodb+srv://testUser:testUser12345678@nodetutblog.kxfwi.mongodb.net/node-blog-test?retryWrites=true&w=majority";
+mongoose
+    .connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then((result) => app.listen(3000))
+    .catch((err) => console.log(err));
 
 // register view engine
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
 // routes
-app.get('/', (req, res) => {
-  res.redirect('/blogs');
+app.get("/", (req, res) => {
+    res.redirect("/blogs");
 });
 
-app.get('/about', (req, res) => {
-  res.render('about', { title: 'About' });
+app.get("/about", (req, res) => {
+    res.render("about", { title: "About" });
+});
+
+// login logout test route
+app.get("/signup", (req, res) => {
+    res.render("signup", { title: "Sign up" });
+});
+app.get("/login", (req, res) => {
+    res.render("login", { title: "Login" });
 });
 
 // blog routes
-app.use('/blogs', blogRoutes)
+app.use("/blogs", blogRoutes);
 
 // 404 page
 app.use((req, res) => {
-  res.status(404).render('404', { title: '404' });
+    res.status(404).render("404", { title: "404" });
 });
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,19 @@
 module.exports = {
-  future: {
-    // removeDeprecatedGapUtilities: true,
-    // purgeLayersByDefault: true,
-  },
-  purge: [],
-  theme: {
-    extend: {
-      fontFamily: {
-        custom: ['Roboto'],
-        nav: ['Roboto Mono']
-      }
+    future: {
+        removeDeprecatedGapUtilities: true,
+        purgeLayersByDefault: true,
     },
-  },
-  variants: {},
-  plugins: [],
-}
+    purge: [],
+    theme: {
+        extend: {
+            fontFamily: {
+                inter: ["Inter"],
+            },
+            spacing: {
+                84: "21rem",
+            },
+        },
+    },
+    variants: {},
+    plugins: [],
+};
