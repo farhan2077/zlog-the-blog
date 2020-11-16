fa45c1b57b175000edab1417ec8c84928b568965
Farhan Bin Amin <farhanbinamin@gmail.com>
2020-11-16 10:18:51 +0600

    changed port

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const morgan = require("morgan");
 const mongoose = require("mongoose");
 const cookieParser = require('cookie-parser');
+const PORT = process.env.PORT || 3000;
 
 const { requireAuth, checkUser } = require("./middleware/authMiddleware");
 const { findByIdAndDelete } = require("./models/blog");
@@ -22,7 +23,7 @@ const dbURI =
     "mongodb+srv://testUser:testUser12345678@nodetutblog.kxfwi.mongodb.net/node-blog-test?retryWrites=true&w=majority";
 mongoose
     .connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then((result) => app.listen(3000))
+    .then((result) => app.listen(PORT))
     .catch((err) => console.log(err));
 
 // register view engine
