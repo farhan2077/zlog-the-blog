d7f371bc16b7bb715a8b8dba0a8db8c904421bc8
Farhan Bin Amin <farhanbinamin@gmail.com>
2021-01-12 11:21:15 +0600

    upgraded tailwindcss to v2

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,4 @@
 module.exports = {
-    future: {
-        removeDeprecatedGapUtilities: true,
-        purgeLayersByDefault: true,
-    },
     purge: {
         enabled: true,
         content: ["./views/**/*.ejs"],
